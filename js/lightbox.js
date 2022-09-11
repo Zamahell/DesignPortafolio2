0a3685d99dbc37a147457641cd40b8f0819fed8b
Zamahell <brau.herrerahuerta@gmail.com>
2022-09-11 05:35:31 -0300

    First Commit

---
+++ js/lightbox.js
@@ -0,0 +1,7 @@
+const imagenes = document.querySelector('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight= document.querySelector('.imagen-light')
+
+console.log(imagenes)
+console.log(imagenesLight)
+console.log(contenedorLight)
